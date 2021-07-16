88fa51a422083201b37f636e13ad50511d6379da
Deepak Tomar <deepaktomar@Deepaks-MacBook-Air.local>
2021-07-17 02:11:39 +0530

    TypeScript Initial Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SetupTypeScriptProject
+Initialize
+Command : npm init -y
+
+Webpack
+Command : npm install webpack webpack-cli --save-dev
+
+TS Loader
+Command : npm install typescript ts-loader --save-dev
+
+Serveral Commands Parallely
+Command : npm install npm-run-all --save-dev
+
+Live Server
+Command : npm install live-server --save-dev
---
+++ dist/index.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+var Helper = /** @class */ (function () {
+    function Helper() {
+    }
+    Helper.prototype.component = function () {
+        var element = document.createElement('div');
+        element.innerHTML = 'Hello, how are you? I am written in typescript.';
+        return element;
+    };
+    return Helper;
+}());
+document.body.appendChild(new Helper().component());
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90ZXRyaXMvLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtJQUFBO0lBT0EsQ0FBQztJQUxVLDBCQUFTLEdBQWhCO1FBQ0ksSUFBTSxPQUFPLEdBQUcsUUFBUSxDQUFDLGFBQWEsQ0FBQyxLQUFLLENBQUM7UUFDN0MsT0FBTyxDQUFDLFNBQVMsR0FBRyxpREFBaUQ7UUFDckUsT0FBTyxPQUFPO0lBQ2xCLENBQUM7SUFDTCxhQUFDO0FBQUQsQ0FBQztBQUVELFFBQVEsQ0FBQyxJQUFJLENBQUMsV0FBVyxDQUFDLElBQUksTUFBTSxFQUFFLENBQUMsU0FBUyxFQUFFLENBQUMiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBIZWxwZXIge1xuXG4gICAgcHVibGljIGNvbXBvbmVudCAoKSA6IEhUTUxEaXZFbGVtZW50IHtcbiAgICAgICAgY29uc3QgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpXG4gICAgICAgIGVsZW1lbnQuaW5uZXJIVE1MID0gJ0hlbGxvLCBob3cgYXJlIHlvdT8gSSBhbSB3cml0dGVuIGluIHR5cGVzY3JpcHQuJ1xuICAgICAgICByZXR1cm4gZWxlbWVudFxuICAgIH1cbn1cblxuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChuZXcgSGVscGVyKCkuY29tcG9uZW50KCkpIl0sInNvdXJjZVJvb3QiOiIifQ==
